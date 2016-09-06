128ff6d336ae906be9813608eee4e258660a964b
miguelescanero <miguel.escanero1@gmail.com>
2016-09-06 18:54:33 -0500

    entrega de trabajo

--- Exercises/Miguel Armando/test Exercise/spec/script-spec.js
+++ Exercises/Miguel Armando/test Exercise/spec/script-spec.js
@@ -1,5 +1,22 @@
 function suma(numero1,numero2){
   var res = numero1 + numero2;
+
+  if(isNaN(res)){
+    res=false;
+  }
+  if(numero1===null&&numero2===null){
+    var res=numero1+numero2;
+  }
+  if (numero1===NaN&&numero2===null) {
+    var res=numero1+numero2;
+  }
+  if(typeof(numero1)==="number"&&typeof(numero2)==="number"){
+    var res =numero1+numero2;
+  }
+  if (numero1===Infinity||numero2===Infinity) {
+    var res=numero1+numero2;
+}
+
   return res;
 }
 describe("testing suma", function(){
@@ -7,32 +24,52 @@ describe("testing suma", function(){
     var res = suma(2,2);
       expect(res).toBe(res);
   });
+
   it("espero que 3 + 3 sea 6 ", function(){
     var res = suma (3,3);
     expect(res).toBe(6);
   })
-  it("espero que NaN + null sea NaN ", function(){
-    var res = suma (NaN,null);
-    expect(res).toBe(NaN);
+
+  it("espero que NaN + NaN sea NaN ", function(){
+    var res = suma (NaN,NaN);
+    expect(res).toBeNaN();
   })
+
+  
   it("espero que NaN + 2 sea NaN ", function(){
     var res = suma (NaN,2);
-    expect(res).toBe(NaN);
+    expect(res).toBeNaN();
   })
 });
 
 function resta(numero1,numero2){
   var res = numero1 - numero2;
-  return res;
+
+  if(isNaN(res)){
+    res=false;
+  }
+  if(numero1===null&&numero2===null){
+    var res=numero1-numero2;
+  }
+  if (numero1===NaN&&numero2===null) {
+    var res=numero1-numero2;
+  }
+  if(typeof(numero1)==="number"&&typeof(numero2)==="number"){
+    var res =numero1-numero2;
+  }
+  if (numero1===Infinity||numero2===Infinity) {
+    var res=numero1-numero2;
+}
+return res;
 }
 describe("testing resta", function(){
   it("espero que 2 - 2 sea 0", function (){
     var res = resta(2,2);
       expect(res).toBe(0);
   });
-    it("espero que 2 - NaN sea NaN", function (){
-      var res = resta(2,NaN);
-        expect(res).toBe(NaN);
+    it("espero que NaN - NaN sea NaN", function (){
+      var res = resta(NaN,NaN);
+        expect(res).toBeNaN();
     });
 
     });
@@ -48,12 +85,17 @@ describe("testing resta", function(){
       });
       it("espero que 3 * NaN sea NaN ", function(){
         var res = multiplicacion (3,NaN);
-        expect(res).toBe(NaN);
+        expect(res).toBeNaN();
       })
       it("espero que null * null sea 0 ", function(){
         var res = multiplicacion (null,null);
         expect(res).toBe(0);
       })
+      it("espero que NaN * NaN sea NaN ", function(){
+        var res = multiplicacion (NaN,NaN);
+        expect(res).toBeNaN();
+      })
+
     });
 
     function division(numero1,numero2){
@@ -68,6 +110,14 @@ describe("testing resta", function(){
       });
       it("espero que 3 / NaN sea NaN ", function(){
         var res = division (3,NaN);
-        expect(res).toBe(NaN);
+        expect(res).toBeNaN();
+      })
+      it("espero que 1 / NaN sea NaN ", function(){
+        var res = division (1,NaN);
+        expect(res).toBeNaN();
+      })
+      it("espero que NaN / NaN sea NaN ", function(){
+        var res = division (NaN,NaN);
+        expect(res).toBeNaN();
       })
     });
