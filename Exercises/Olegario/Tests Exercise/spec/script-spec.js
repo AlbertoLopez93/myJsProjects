2aa9442f2ca2f4f8bd76d349275529290220edbf
olegario fernandez <olegario_90_shock@hotmail.com>
2016-09-06 18:51:49 -0500

    entrega

--- Exercises/Olegario/Tests Exercise/spec/script-spec.js
+++ Exercises/Olegario/Tests Exercise/spec/script-spec.js
@@ -1,31 +1,39 @@
 function suma(numero1, numero2) {
   var result=false;
-  if (Number.isInteger(numero1)&&Number.isInteger(numero2)) {
-       result= numero1+numero2;
+  if ((typeof(numero1)==="number")&&(typeof(numero2)==="number")) {
+       result=numero1+numero2;
+       result= result.toPrecision(10);
+       result= parseFloat(result);
       }
   return result;
 }
 
 function resta(numero1, numero2) {
    var result=false;
-   if (Number.isInteger(numero1)&&Number.isInteger(numero2)) {
-        result= numero1-numero2;
+   if ((typeof(numero1)==="number")&&(typeof(numero2)==="number")) {
+        result=numero1-numero2;
+        result= result.toPrecision(10);
+        result= parseFloat(result);
        }
  return result;
 }
 
 function multiplicacion(numero1, numero2) {
    var result=false;
-   if (Number.isInteger(numero1)&&Number.isInteger(numero2)) {
-        result= numero1*numero2;
+   if ((typeof(numero1)==="number")&&(typeof(numero2)==="number")) {
+        result=numero1*numero2;
+        result= result.toPrecision(10);
+        result= parseFloat(result);
        }
  return result;
 }
 
 function divicion(numero1, numero2) {
    var result=false;
-   if (Number.isInteger(numero1) && Number.isInteger(numero2)) {
-        result= numero1/numero2;
+   if ((typeof(numero1)==="number")&&(typeof(numero2)==="number")) {
+        result=numero1/numero2;
+        result= result.toPrecision(10);
+        result= parseFloat(result);
        }
  return result;
 }
@@ -36,15 +44,55 @@ describe("Testing Suma",function(){//Conjunto de funciones de prueba
         expect(res).toBe(4);
      });
 
-     it("Espero que 3 + 3 sea = 6", function() {//Cada una de las pruebas
-           var res=suma(3,3);
-           expect(res).toBe(6);
-        });
+  it("Espero que 3 + 3 sea = 6", function() {//Cada una de las pruebas
+         var res=suma(3,3);
+         expect(res).toBe(6);
+     });
 
-     it("Espero que 5 + 5 sea = 10", function() {//Cada una de las pruebas
+  it("Espero que 5 + 5 sea = 10", function() {//Cada una de las pruebas
            var res=suma(5,5);
            expect(res).toBe(10);
-        });
+     });
+
+  it("Espero que [1,2] + 5 sea = false", function() {//Cada una de las pruebas
+          var res=suma([1,2],5);
+          expect(res).toBe(false);
+     });
+
+  it("Espero que 1 + {} sea = false", function() {//Cada una de las pruebas
+         var res=suma(1,{});
+         expect(res).toBe(false);
+     });
+
+  it("Espero que NaN + NaN sea = NaN", function() {//Cada una de las pruebas
+          var res=suma(NaN,NaN);
+          expect(res).toBeNaN();
+     });
+
+  it("Espero que undefined + false sea = false", function() {//Cada una de las pruebas
+         var res=suma(undefined,false);
+         expect(res).toBe(false);
+     });
+
+  it("Espero que null + NaN sea = false", function() {//Cada una de las pruebas
+          var res=suma(null,NaN);
+          expect(res).toBe(false);
+     });
+
+  it("Espero que Array + 5 sea = false", function() {//Cada una de las pruebas
+        var res=suma(Array,5);
+        expect(res).toBe(false);
+     });
+
+  it("Espero que Infinity + Infinity = Infinity", function() {//Cada una de las pruebas
+         var res=suma(1,Infinity);
+         expect(res).toBe(Infinity);
+     });
+
+  it("Espero que 0.1 + 0.2 = 0.3", function() {//Cada una de las pruebas
+          var res=suma(0.1,0.2);
+          expect(res).toBe(0.3);
+     });
 });
 
 
@@ -74,9 +122,14 @@ describe("Testing Par Funcion De Resta",function(){//Conjunto de funciones de pr
            expect(res).toBe(false);
         });
 
-     it("Espero que 1 - NaN sea = false", function() {//Cada una de las pruebas
+     it("Espero que 1 - NaN sea = NaN", function() {//Cada una de las pruebas
            var res=resta(1,NaN);
-           expect(res).toBe(false);
+           expect(res).toBeNaN();
+        });
+
+     it("Espero que  1 - Infinity sea = -Infinity", function() {//Cada una de las pruebas
+          var res=resta(1,Infinity);
+          expect(res).toBe(-Infinity);
         });
 });
 
@@ -107,9 +160,9 @@ describe("Testing Par Funcion De Multiplicacion",function(){//Conjunto de funcio
            expect(res).toBe(false);
         });
 
-     it("Espero que 1 * NaN sea = false", function() {//Cada una de las pruebas
+     it("Espero que 1 * NaN sea = NaN", function() {//Cada una de las pruebas
            var res=multiplicacion(1,NaN);
-           expect(res).toBe(false);
+           expect(res).toBeNaN();
         });
 });
 
@@ -127,7 +180,7 @@ describe("Testing Par Funcion De Divicion",function(){//Conjunto de funciones de
 
      it("Espero que 0 / 0 sea = NaN", function() {//Cada una de las pruebas
            var res=divicion(0,0);
-           expect(res).toBe(isNaN(NaN));
+           expect(res).toBeNaN();
         });
 
      it("Espero que A / 2 sea = false", function() {//Cada una de las pruebas
@@ -140,9 +193,9 @@ describe("Testing Par Funcion De Divicion",function(){//Conjunto de funciones de
            expect(res).toBe(false);
         });
 
-     it("Espero que 1 / NaN sea = false", function() {//Cada una de las pruebas
+     it("Espero que 1 / NaN sea = NaN", function() {//Cada una de las pruebas
            var res=divicion(1,NaN);
-           expect(res).toBe(false);
+           expect(res).toBeNaN();
         });
 
      it("Espero que 1 / false sea = false", function() {//Cada una de las pruebas
