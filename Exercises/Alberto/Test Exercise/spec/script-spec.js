ba31c3d9bce490830afcefec188457303a679760
AlbertoLopez93 <beto.lv.teptaitlan@gmail.com>
2016-09-06 18:37:53 -0500

    More testings

--- Exercises/Alberto/Test Exercise/spec/script-spec.js
+++ Exercises/Alberto/Test Exercise/spec/script-spec.js
@@ -2,37 +2,21 @@ function suma(num1,num2) {
   num1=parseFloat(num1);
   num2=parseFloat(num2);
   var res=num1+num2;
-  if(Number.isNaN(res))
-  {
-    return false;
-  }
   return res;
 }
 
 function resta(num1,num2) {
   var res=num1-num2;
-  if(Number.isNaN(res))
-  {
-    return false;
-  }
   return res;
 }
 
 function multiplicacion(num1,num2) {
   var res=num1*num2;
-  if(Number.isNaN(res))
-  {
-    return false;
-  }
   return res;
 }
 
 function division(num1,num2) {
   var res=num1/num2;
-  if(Number.isNaN(res))
-  {
-    return false;
-  }
   return res;
 }
 
@@ -53,9 +37,17 @@ describe("Testing suma", function() {
     var res= suma(1.2,'1.1');
     expect(res).toBe(2.3);
   });
-  it("I wait 'hello'+3 equal false", function(){
+  it("I wait 'hello'+3 equal NaN", function(){
     var res= suma('hello',3);
-    expect(res).toBe(false);
+    expect(res).toBeNaN;
+  });
+  it("I wait 8+'' equal NaN", function(){
+    var res= suma(8,'');
+    expect(res).toBeNaN;
+  });
+  it("I wait 3+Infinity equal Infinity", function(){
+    var res= suma(3,Infinity);
+    expect(res).toBe(Infinity);
   });
 });
 
@@ -72,9 +64,25 @@ describe("Testing resta", function() {
     var res= resta(5,7);
     expect(res).not.toBe(2);
   });
-  it("I wait 'bye'-7 equal false", function(){
+  it("I wait 5.7-1.7 equal 4", function(){
+    var res= resta(5.7,1.7);
+    expect(res).toBe(4);
+  });
+  it("I wait 'bye'-7 equal NaN", function(){
     var res= resta('bye',7);
-    expect(res).toBe(false);
+    expect(res).toBeNaN;
+  });
+  it("I wait true-false equal NaN", function(){
+    var res= resta(true,false);
+    expect(res).toBeNaN;
+  });
+  it("I wait Infinity-1 equal Infinity", function(){
+    var res= resta(Infinity,1);
+    expect(res).toBe(Infinity);
+  });
+  it("I wait 3-Infinity equal NaN", function(){
+    var res= resta(3,Infinity);
+    expect(res).toBeNaN;
   });
 });
 
@@ -91,13 +99,25 @@ describe("Testing multiplicacion", function() {
     var res= multiplicacion(0.5,2);
     expect(res).toBe(1);
   });
-  it("I wait 'welcome'*2 equal false", function(){
+  it("I wait 'welcome'*2 equal NaN", function(){
     var res= multiplicacion('welcome',2);
-    expect(res).toBe(false);
+    expect(res).toBeNaN;
   });
   it("I wait 9*undefined not equal zero", function(){
     var res= multiplicacion(9,undefined);
     expect(res).not.toBe(0);
+  });
+  it("I wait 9*'' equal NaN", function(){
+    var res= multiplicacion(9,'');
+    expect(res).toBeNaN;
+  });
+  it("I wait 0*0 equal 0", function(){
+    var res= multiplicacion(0,0);
+    expect(res).toBe(0);
+  });
+  it("I wait Infinity*Infinity equal Infinity", function(){
+    var res= multiplicacion(Infinity,Infinity);
+    expect(res).toBe(Infinity);
   });
 });
 
@@ -114,8 +134,28 @@ describe("Testing division", function() {
     var res= division(4,0);
     expect(res).toBe(Infinity);
   });
-  it("I wait 'four'/'one' equal false", function(){
+  it("I wait 4/0.2 equal 20", function(){
+    var res= division(4,0.2);
+    expect(res).toBe(20);
+  });
+  it("I wait 24/50 equal less than one", function(){
+    var res= division(24,50);
+    expect(res).toBeLessThan(1);
+  });
+  it("I wait 'four'/'one' equal NaN", function(){
     var res= division('four','one');
-    expect(res).toBe(false);
+    expect(res).toBeNaN;
+  });
+  it("I wait Infinity/2 equal NaN", function(){
+    var res= division(Infinity/2);
+    expect(res).toBeNaN;
+  });
+  it("I wait null/2 not equal zero", function(){
+    var res= division(Infinity/2);
+    expect(res).not.toBe(0);
+  });
+  it("I wait Infinity/Infinity equal NaN", function(){
+    var res= division(Infinity/Infinity);
+    expect(res).toBeNaN;
   });
 });
