6ebd0e71624e68e1baab684dfd29e4e928937b02
Lorenzo Peña Álvarez <lorenzo.pena@gmail.com>
2016-09-06 16:02:38 -0500

    Created Tests Exercise folder, script and spec

---
+++ Exercises/Trainers/Tests Exercise/spec/script-spec.js
@@ -0,0 +1,17 @@
+function suma(numero1, numero2) {
+    if (typeof numero1 === 'string' || typeof numero2 === 'string') {
+        return NaN;
+    }
+    var res = numero1 + numero2;
+    return res;
+}
+
+function resta() {}
+
+describe("Testing suma", function () {
+    it("Espero que dos cadenas sea NaN", function () {
+        var res = suma("hola","mundo");
+        var checkForNaN = isNaN(res);
+        expect(checkForNaN).toBe(true);
+    });
+});
