b0b0663e321faffa216bffd0f7700890b56ab4c3
Gerardo Ruiz Santana <gera.ing.comp@gmail.com>
2016-09-06 18:58:55 -0500

    Funcion suma

--- Exercises/Gerardo/Tests Exercise/spec/script-spec.js
+++ Exercises/Gerardo/Tests Exercise/spec/script-spec.js
@@ -1,25 +1,37 @@
 function suma(numero1, numero2) {
-  if(isNaN(numero1) || isNaN(numero2)) {
-    return NaN;
+  if(typeof numero1 === 'number' && numero2 === 'number') {
+    return numero1 + numero2;
   }
-  return numero1 + numero2;
+  return undefined;
 }
 
-function resta(numero1, numero2) {
-  return numero1 - numero2;
-}
-
-function multiplicacion(numero1, numero2) {
-  return numero1 * numero2;
-}
-
-function division(numero1, numero2) {
-  return numero1 / numero2;
-}
-
-describe ("", function () {
-  it("Suma de dos caracteres", function () {
+describe ("Funcion de suma", function () {
+  it("undefined + algo = undefined", function () {
     var res = suma('a', 'b');
-    expect(res).toBe(NaN);
+    expect(res).toBe(undefined);
+  });
+  it("array + algo =  undefined", function () {
+    var res = suma([], []);
+    expect(res).toBe(undefined);
+  });
+  it("objeto + algo = undefined", function () {
+    var res = suma({}, 1);
+    expect(res).toBe(undefined);
+  });
+  it("null + algo = undefined", function () {
+    var res = suma(null, 1);
+    expect(res).toBe(undefined);
+  });
+  it("boolean + algo = undefined", function () {
+    var res = suma(true, 1);
+    expect(res).toBe(undefined);
+  });
+  it("string + algo = undefined", function () {
+    var res = suma('a', 1);
+    expect(res).toBe(undefined);
+  });
+  it("funcion + algo = undefined", function () {
+    var res = suma(function() {}, 1);
+    expect(res).toBe(undefined);
   });
 });
