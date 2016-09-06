062ebe0fa1db121305cfdbc4fbaa2a6fee75d86a
Isaac Ramírez <iramirezc@live.com.mx>
2016-09-06 16:39:33 -0500

    Tests for resta, multiplicación and división

--- Exercises/Isaac/Tests Exercise/spec/script-spec.js
+++ Exercises/Isaac/Tests Exercise/spec/script-spec.js
@@ -1,7 +1,6 @@
 function suma(n1, n2) {
-    return (+n1) + (+n2);
+    return Number(((+n1) + (+n2)).toPrecision(10));
 }
-
 
 function resta(n1, n2) {
     return n1 - n2;
@@ -15,11 +14,9 @@ function division(n1, n2) {
     return n1 / n2;
 }
 
-//describe
+// Suma
 describe("Testing suma", function () {
-    //it
     it("Expect 2 + 2 to be 4", function () {
-        //expect
         expect(suma(2, 2)).toBe(4);
     });
 
@@ -48,12 +45,47 @@ describe("Testing suma", function () {
     });
 });
 
+// Resta
 describe("Testing resta", function () {
     it("Expect 10 - 5 to be 5", function () {
         expect(resta(10, 5)).toBe(5);
     });
     
     it("Expect 5 - 10 to be 5", function () {
-        expect(resta(10, 5)).toBe(5);
+        expect(resta(5, 10)).toBe(-5);
+    });
+    
+    it("Expect 'b' - 10 to be NaN", function () {
+        expect(String(resta('b', 10))).toBe('NaN');
+    });
+});
+
+// Multiplicación
+describe("Testing multiplicacion", function () {
+    it("Expect 10 * 5 to be 50", function () {
+        expect(multiplicacion(10, 5)).toBe(50);
+    });
+    
+    it("Expect 2 * 10 to be 20", function () {
+        expect(multiplicacion(2, 10)).toBe(20);
+    });
+    
+    it("Expect 'b' * 10 to be NaN", function () {
+        expect(String(multiplicacion('b', 10))).toBe('NaN');
+    });
+});
+
+// División
+describe("Testing division", function () {
+    it("Expect 10 / 5 to be 2", function () {
+        expect(division(10, 5)).toBe(2);
+    });
+    
+    it("Expect 2 / 10 to be 0.2", function () {
+        expect(division(2, 10)).toBe(0.2);
+    });
+    
+    it("Expect 'b' / 10 to be NaN", function () {
+        expect(String(division('b', 10))).toBe('NaN');
     });
 });
