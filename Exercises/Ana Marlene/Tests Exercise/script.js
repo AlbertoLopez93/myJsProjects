5f33f6afad637bf0c2a55971e8bff554a0176c14
Marlene <marlene_lozano_94@live.com.mx>
2016-09-06 16:10:22 -0500

    carpeta Tests

---
+++ Exercises/Ana Marlene/Tests Exercise/script.js
@@ -0,0 +1,18 @@
+//script.js
+function suma(num1,num2){
+
+}
+
+module.exports = suma;
+
+function resta(num1,num2){
+
+}
+
+module.exports = resta;
+
+function mul(num1,num2){
+
+}
+
+module.exports = mul;
