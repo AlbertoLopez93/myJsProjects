f4150ba32d58be56d0b9dcbc5366fd51d2318a0b
sergio rivera <telesforociriaco@gmail.com>
2016-09-07 18:52:36 -0500

    vance 2

--- Exercises/Sergio/Logic exercises 1/spec/script-spec.js
+++ Exercises/Sergio/Logic exercises 1/spec/script-spec.js
@@ -91,6 +91,7 @@ function change(str) {
   return r;
 }
 
+////// EXERCISE ONE //////
 
 function isAnagram(str1,str2) {
   let cad1,cad2,r=false;
@@ -105,52 +106,189 @@ function isAnagram(str1,str2) {
               cad1[j]=""; cont++;break find;
            }
          }
-         }
-       if (cont=== str1.length) {
-         r=true;
        }
-       }
+      if (cont=== str1.length) {
+        r=true;
+      }
     }
-  return r;
   }
+  return r;
+}
 
+//////EXERCISE DOS //////
 
-  describe("Testing For Anagrams",function(){//Conjunto de funciones de prueba
-    it("Espero que Roma y amOr sea = true", function() {//Cada una de las pruebas
-      var res=isAnagram("Roma","aMor");
-      expect(res).toBe(true);
-    });
+function convertSpaces(str) {
+let cad=str.split(" "),result="";
 
-    it("Espero que 2 y 2 sea = false", function() {//Cada una de las pruebas
-      var res=isAnagram(2,2);
-      expect(res).toBe(false);
-    });
+ for (var i = 0; i < cad.length; i++) {
+      if ((i>0)&&(i<cad.length)) {
+        result+="%20"
+      }
+      result+=cad[i];
+ }
+ return result;
+}
 
-    it("Espero que [] y [] sea = false", function() {//Cada una de las pruebas
-      var res=isAnagram([],[]);
-      expect(res).toBe(false);
-    });
+////////EXERCISE THREE  //////
+function rounded(numer) {
+let numero1 = numer;
+if(typeof(numero1)==="string"){
+  return false;
+}
+if(numero1>Number.MAX_VALUE){
+  return Infinity;
+}
+if(numero1<-Number.MAX_VALUE){
+  return -Infinity;
+}
+let strnum=numero1.toString();
+let cad=strnum.split("."),r,round=0;
+let flo=cad[1].split("");
+let n1=cad[0]+flo[0]+flo[1];
+if (flo[2]>=5) {
+  r=parseInt(n1)+1;
+  }else{
+  r=parseInt(n1);
+  }
+  flo=r.toString().split(""); r="";
+  for (let i = 0; i < flo.length; i++) {
+    if (i===(flo.length-2)) {
+      if (i===0) {
+        r+="0";
+      }
+    r+=".";
+  }
+    r+=flo[i];;
+  }
+ return parseFloat(r);
+}
 
-    it("Espero que {} y {} sea = false", function() {//Cada una de las pruebas
-      var res=isAnagram({},{});
-      expect(res).toBe(false);
-    });
+/////// EXERCISE FOUR ////////
+function getTotal(days){
+  if(typeof(days)==="string"){
+    return false;
+  }
+  if(days>Number.MAX_VALUE){
+    return Infinity;
+  }
+  if(days<-Number.MAX_VALUE){
+    return -Infinity;
+  }
+let r = 0;
+  if(days<3){
+    r = ((days)*40);
+  }else if(days<7) {
+    r = ((days*40)-20);
+  }else{
+    r = ((days*40)-50);
+  }
+  return r;
+}
 
-    it("Espero que Infinity y null sea = false", function() {//Cada una de las pruebas
-      var res=isAnagram(Infinity,null);
-      expect(res).toBe(false);
-    });
-    it("Espero que '' y '' sea = false", function() {//Cada una de las pruebas
-      var res=isAnagram('','');
-      expect(res).toBe(true);
-    });
-    it("Espero que null y '' sea = false", function() {//Cada una de las pruebas
-      var res=isAnagram(null,'');
-      expect(res).toBe(false);
-    });
-    it("Espero que 'Infiltrado' y 'Infiltrada' sea = false", function() {//Cada una de las pruebas
-      var res=isAnagram("Infiltrado","Infiltrada");
-      expect(res).toBe(false);
-    });
-
+describe("Testing For Anagrams",function(){//Conjunto de funciones de prueba
+  it("Espero que Roma y amOr sea = true", function() {//Cada una de las pruebas
+    var res=isAnagram("Roma","aMor");
+    expect(res).toBe(true);
   });
+
+  it("Espero que 2 y 2 sea = false", function() {//Cada una de las pruebas
+    var res=isAnagram(2,2);
+    expect(res).toBe(false);
+  });
+
+  it("Espero que [] y [] sea = false", function() {//Cada una de las pruebas
+    var res=isAnagram([],[]);
+    expect(res).toBe(false);
+  });
+
+  it("Espero que {} y {} sea = false", function() {//Cada una de las pruebas
+    var res=isAnagram({},{});
+    expect(res).toBe(false);
+  });
+
+  it("Espero que Infinity y null sea = false", function() {//Cada una de las pruebas
+    var res=isAnagram(Infinity,null);
+    expect(res).toBe(false);
+  });
+  it("Espero que '' y '' sea = false", function() {//Cada una de las pruebas
+    var res=isAnagram('','');
+    expect(res).toBe(true);
+  });
+  it("Espero que null y '' sea = false", function() {//Cada una de las pruebas
+    var res=isAnagram(null,'');
+    expect(res).toBe(false);
+  });
+  it("Espero que 'Infiltrado' y 'Infiltrada' sea = false", function() {//Cada una de las pruebas
+    var res=isAnagram("Infiltrado","Infiltrada");
+    expect(res).toBe(false);
+  });
+  it("Espero que 'olegario armario' sea = olegario%20armario", function() {//Cada una de las pruebas
+    var res=convertSpaces("olegario  vive en el armario");
+    expect(res).toBe("olegario%20%20vive%20en%20el%20armario");
+  });
+  it("Espero que 'olegario armario' sea = olegario%20armario", function() {//Cada una de las pruebas
+    var res=convertSpaces("infiltrado    ");
+    expect(res).toBe("infiltrado%20%20%20%20");
+  });
+  it("Espero que internado sea = internado", function() {//Cada una de las pruebas
+    var res=convertSpaces("internado");
+    expect(res).toBe("internado");
+  });
+  it("Espero que 29.5689 sea = 29.57", function() {//Cada una de las pruebas
+    var res=rounded(29.5689);
+    expect(res).toBe(29.57);
+  });
+  it("Espero que 29.5649 sea = 29.56", function() {//Cada una de las pruebas
+    var res=rounded(29.5649);
+    expect(res).toBe(29.56);
+  });
+  it("Espero que 29.5674765 sea = false", function() {//Cada una de las pruebas
+    var res=rounded(29.5674765);
+    expect(res).toBe(29.57);
+  });
+  it("Espero que '13.8888' sea = NaN", function() {//Cada una de las pruebas
+    var res=rounded("13.8888");
+    expect(res).toBe(false);
+  });
+  it("Espero que {13.9955} sea = NaN", function() {//Cada una de las pruebas
+    var res=rounded("{13.9955}");
+    expect(res).toBe(false);
+  });
+  it("Espero que Infinity sea = false", function() {//Cada una de las pruebas
+    var res=rounded(Infinity);
+    expect(res).toBe(Infinity);
+  });
+  it("Espero que Infinity sea = false", function() {//Cada una de las pruebas
+    var res=rounded(-Infinity);
+    expect(res).toBe(-Infinity);
+  });
+  //FOUR
+  it("Espero que 29.5689 sea = 29.57", function() {//Cada una de las pruebas
+    var res=getTotal(2);
+    expect(res).toBe(80);
+  });
+  it("Espero que 3 sea = 100", function() {//Cada una de las pruebas
+    var res=getTotal(3);
+    expect(res).toBe(100);
+  });
+  it("Espero que 8 sea = 270", function() {//Cada una de las pruebas
+    var res=getTotal(8);
+    expect(res).toBe(270);
+  });
+  it("Espero que '13.8888' sea = NaN", function() {//Cada una de las pruebas
+    var res=getTotal("13.8888");
+    expect(res).toBe(false);
+  });
+  it("Espero que {13.9955} sea = NaN", function() {//Cada una de las pruebas
+    var res=getTotal("{13.9955}");
+    expect(res).toBe(false);
+  });
+  it("Espero que Infinity sea = Infinity", function() {//Cada una de las pruebas
+    var res=getTotal(Infinity);
+    expect(res).toBe(Infinity);
+  });
+  it("Espero que Infinity sea = -Infinity", function() {//Cada una de las pruebas
+    var res=getTotal(-Infinity);
+    expect(res).toBe(-Infinity);
+  });
+});
