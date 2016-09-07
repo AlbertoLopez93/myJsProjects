f35bcac020d113146c9fa053d1ef8baa4c244a59
Marlene <marlene_lozano_94@live.com.mx>
2016-09-06 19:01:04 -0500

    NO ACABE ;(

--- Exercises/Ana Marlene/Tests Exercise/script.js
+++ Exercises/Ana Marlene/Tests Exercise/script.js
@@ -1,11 +1,12 @@
 //script.js
 function suma(num1,num2){
-
+var resultado=0;
 }
 
 module.exports = suma;
 
 function resta(num1,num2){
+  var resultado=0
 
 }
 
@@ -16,3 +17,8 @@ function mul(num1,num2){
 }
 
 module.exports = mul;
+function division(num1,num2){
+
+}
+
+module.exports = division;
