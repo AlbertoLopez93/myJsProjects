f0bac0c23d6e4b68f7a1458a11c3f7363f5c8cb3
Axl-Nolasco <axl_nolasco@hotmail.com>
2016-09-08 01:54:45 -0500

    Test for Exercises 1 to 7

--- Exercises/Hector/Logic Exercises 1/spec/script-spec.js
+++ Exercises/Hector/Logic Exercises 1/spec/script-spec.js
@@ -1,7 +1,5 @@
 "use strict"
 function isAnagram(str1, str2){
-
-
     if(typeof(str1) === 'number' || typeof(str2) === 'number'){
         return false;
     }
@@ -17,24 +15,21 @@ function isAnagram(str1, str2){
     if(typeof(str1) !== 'string' || typeof(str2) !== 'string'){
         return false;
     }
-
-
-
-	var x = str1.split("");
-	var y = str2.split("");
-	if(str1.length !== str2.length){
-	    return false;
-	}
-	x = toNum(x);
-	y = toNum(y);
-	x = bubble(x);
-	y = bubble(y);
-	for(let i=0; i<x.length;i++){
-		if(x[i]!==y[i]){
-			return false;
-		}
-	}
-	return true;
+    var x = str1.split("");
+    var y = str2.split("");
+    if(str1.length !== str2.length){
+        return false;
+    }
+    x = toNum(x);
+    y = toNum(y);
+    x = bubble(x);
+    y = bubble(y);
+    for(let i=0; i<x.length;i++){
+        if(x[i]!==y[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
 function bubble(x){
@@ -54,121 +49,121 @@ function bubble(x){
 }
 
 function toNum(x){
-	for(let i=0; i<x.length; i++){
-		switch (x[i]){
-			case 'A':
-			case 'a':
-				x[i] = 1;
-				break;
-			case 'B':
-			case 'b':
-				x[i] = 2;
-				break;
-			case 'C':
-			case 'c':
-				x[i] = 3;
-				break;
-			case 'D':
-			case 'd':
-				x[i] = 4;
-				break;
-			case 'E':
-			case 'e':
-				x[i] = 5;
-				break;
-			case 'F':
-			case 'f':
-				x[i] = 6;
-				break;
-			case 'G':
-			case 'g':
-				x[i] = 7;
-				break;
-			case 'H':
-			case 'h':
-				x[i] = 8;
-				break;
-			case 'I':
-			case 'i':
-				x[i] = 9;
-				break;
-			case 'J':
-			case 'j':
-				x[i] = 10;
-				break;
-			case 'K':
-			case 'k':
-				x[i] = 11;
-				break;
-			case 'L':
-			case 'l':
-				x[i] = 12;
-				break;
-			case 'M':
-			case 'm':
-				x[i] = 13;
-				break;
-			case 'N':
-			case 'n':
-				x[i] = 14;
-				break;
-			case 'Ñ':
-			case 'ñ':
-				x[i] = 15;
-				break;
-			case 'O':
-			case 'o':
-				x[i] = 16;
-				break;
-			case 'P':
-			case 'p':
-				x[i] = 17;
-				break;
-			case 'Q':
-			case 'q':
-				x[i] = 18;
-				break;
-			case 'R':
-			case 'r':
-				x[i] = 19;
-				break;
-			case 'S':
-			case 's':
-				x[i] = 20;
-				break;
-			case 'T':
-			case 't':
-				x[i] = 21;
-				break;
-			case 'U':
-			case 'u':
-				x[i] = 22;
-				break;
-			case 'V':
-			case 'v':
-				x[i] = 23;
-				break;
-			case 'W':
-			case 'w':
-				x[i] = 24;
-				break;
-			case 'X':
-			case 'x':
-				x[i] = 25;
-				break;
-			case 'Y':
-			case 'y':
-				x[i] = 26;
-				break;
-			case 'Z':
-			case 'z':
-				x[i] = 27;
-				break;
-			default:
-				break;
-		}
-	}
-	return x;
+    for(let i=0; i<x.length; i++){
+        switch (x[i]){
+            case 'A':
+            case 'a':
+            x[i] = 1;
+            break;
+            case 'B':
+            case 'b':
+            x[i] = 2;
+            break;
+            case 'C':
+            case 'c':
+            x[i] = 3;
+            break;
+            case 'D':
+            case 'd':
+            x[i] = 4;
+            break;
+            case 'E':
+            case 'e':
+            x[i] = 5;
+            break;
+            case 'F':
+            case 'f':
+            x[i] = 6;
+            break;
+            case 'G':
+            case 'g':
+            x[i] = 7;
+            break;
+            case 'H':
+            case 'h':
+            x[i] = 8;
+            break;
+            case 'I':
+            case 'i':
+            x[i] = 9;
+            break;
+            case 'J':
+            case 'j':
+            x[i] = 10;
+            break;
+            case 'K':
+            case 'k':
+            x[i] = 11;
+            break;
+            case 'L':
+            case 'l':
+            x[i] = 12;
+            break;
+            case 'M':
+            case 'm':
+            x[i] = 13;
+            break;
+            case 'N':
+            case 'n':
+            x[i] = 14;
+            break;
+            case 'Ñ':
+            case 'ñ':
+            x[i] = 15;
+            break;
+            case 'O':
+            case 'o':
+            x[i] = 16;
+            break;
+            case 'P':
+            case 'p':
+            x[i] = 17;
+            break;
+            case 'Q':
+            case 'q':
+            x[i] = 18;
+            break;
+            case 'R':
+            case 'r':
+            x[i] = 19;
+            break;
+            case 'S':
+            case 's':
+            x[i] = 20;
+            break;
+            case 'T':
+            case 't':
+            x[i] = 21;
+            break;
+            case 'U':
+            case 'u':
+            x[i] = 22;
+            break;
+            case 'V':
+            case 'v':
+            x[i] = 23;
+            break;
+            case 'W':
+            case 'w':
+            x[i] = 24;
+            break;
+            case 'X':
+            case 'x':
+            x[i] = 25;
+            break;
+            case 'Y':
+            case 'y':
+            x[i] = 26;
+            break;
+            case 'Z':
+            case 'z':
+            x[i] = 27;
+            break;
+            default:
+            break;
+        }
+    }
+    return x;
 }
 
 /*Function 2 convertSpaces*/
@@ -179,7 +174,6 @@ function convertSpaces(str){
             cont++;
         }
     }
-
     if(cont === str.length){
         var s = "";
         for(let i=0; i<cont; i++){
@@ -187,29 +181,24 @@ function convertSpaces(str){
         }
         return s;
     }
-
     if(typeof(str) !== "string"){
         return false;
     }
-
-	var s;
-	s = str.split("");
-	for(let i=0; i<s.length-1; i++){
-		if(s[i] === " "){
-			s[i] = "%20";
-		}
-	}
-
-	var res = "";
-	for(let i=0; i<s.length; i++){
-		res += s[i];
-	}
-	return res;
+    var s;
+    s = str.split("");
+    for(let i=0; i<s.length-1; i++){
+        if(s[i] === " "){
+            s[i] = "%20";
+        }
+    }
+    var res = "";
+    for(let i=0; i<s.length; i++){
+        res += s[i];
+    }
+    return res;
 }
 
 /*Function 3 */
-
-
 function rounded(num){
     if(num === true || num === false){
         return false;
@@ -233,52 +222,52 @@ function rounded(num){
         return false;
     }
 
-	num = num.toString();
-	var n = num.split("");
-	var max = n.length;
-	var point = 0;
-	var nueva = [];
-	var last;
-	var number = "";
-	var res = 0;
+    num = num.toString();
+    var n = num.split("");
+    var max = n.length;
+    var point = 0;
+    var nueva = [];
+    var last;
+    var number = "";
+    var res = 0;
 
-	for(let i=0; i<max; i++){
-		if(n[i]=== "."){
-			point = i;
-		}
-	}
+    for(let i=0; i<max; i++){
+        if(n[i]=== "."){
+            point = i;
+        }
+    }
 
-	for(let i=0; i<point+4; i++){
-		nueva[i] = n[i];
-	}
+    for(let i=0; i<point+4; i++){
+        nueva[i] = n[i];
+    }
 
-	last = nueva[nueva.length-1];
-	if(last >= 5){
-		for(let i=0; i<nueva.length; i++){
-			number += nueva[i];
-		}
-		last = 10 - last;
-		last = ".00" + last;
-		res = Number(number) + Number(last);
-		if(res.length===3){
-			res += "0";
-		}
-		if(res%1===0){
-			res += ".00"
-		}
-		return Number(res);
-	}
+    last = nueva[nueva.length-1];
+    if(last >= 5){
+        for(let i=0; i<nueva.length; i++){
+            number += nueva[i];
+        }
+        last = 10 - last;
+        last = ".00" + last;
+        res = Number(number) + Number(last);
+        if(res.length===3){
+            res += "0";
+        }
+        if(res%1===0){
+            res += ".00"
+        }
+        return Number(res);
+    }
     else{
-		for(let i=0; i<nueva.length-1; i++){
-			number += nueva[i];
-	    }
-	    return Number(number);
-	}
+        for(let i=0; i<nueva.length-1; i++){
+            number += nueva[i];
+        }
+        return Number(number);
+    }
 }
 
 /*Function 4*/
 function getTotal(days){
-	var total = days * 40;
+    var total = days * 40;
     if(days === -Infinity){
         return -Infinity;
     }
@@ -301,21 +290,25 @@ function getTotal(days){
     if(!Number(days)){
         return false;
     }
-	if(days >= 3 && days < 7){
-		total -= 20;
-	}
-	if(days >= 7){
-		total -= 50;
-	}
-	return total;
+    if(days >= 3 && days < 7){
+        total -= 20;
+    }
+    if(days >= 7){
+        total -= 50;
+    }
+    return total;
 }
 
 /*Function 5*/
 function squareSum(ary){
+
     if(typeof ary === "number" || ary < 0){
         return false;
     }
     if(ary === null){
+        return false;
+    }
+    if(!ary.length){
         return false;
     }
     if(ary === Infinity){
@@ -327,30 +320,117 @@ function squareSum(ary){
     if(Array(ary) === false ) {
         return false;
     }
-
     if(typeof ary === "string"){
         return false;
     }
-
     if(typeof ary === "number" || ary < 0){
         return false;
     }
-
     for(var i = 0; i< ary.length; i++){
         if(typeof ary[i] !== "number"){
             return false;
         }
     }
-	var x = [];
-	x = ary;
-	var total = 0;
-	var sum = 0;
-	for(let i=0; i<x.length; i++){
-		sum = ary[i] * ary[i];
-		total += sum;
-	}
-	return total;
+    var x = [];
+    x = ary;
+    var total = 0;
+    var sum = 0;
+    for(let i=0; i<x.length; i++){
+        sum = ary[i] * ary[i];
+        total += sum;
+    }
+    return total;
 }
+
+/*Function 6*/
+function isRotation(str1, str2){
+    if(typeof str1 != "string" || typeof str2 != "string" ){
+        return false;
+    }
+    if(str1.match(" ") || str2.match(" ")){
+        return false;
+    }
+    if(str1 === null || str2 === null){
+        return false;
+    }
+    if(str1.length !== str2.length){
+        return false
+    }
+    if(Array.isArray(str1) || Array.isArray(str2)){
+        return false;
+    }
+    if(typeof str1 === "number" || typeof srt2 === "number" ){
+        return false;
+    }
+    var a = str1.split("");
+    var b = str2.split("");
+    var n = [];
+    var temp = "";
+    var x= "", y = "";
+    console.log(a);
+    n = b;
+    console.log(n);
+    //e r b o t t l e w a t
+    for(let i=0; i<a.length; i++){
+        temp = 0;
+        temp = n[0];
+        for(let j=0; j<b.length; j++){
+            n[j] = n[j+1];
+        }
+        n[n.length-1] = temp;
+        x = a.toString();
+        y = n.toString();
+        if(x == y){
+            return true;
+        }
+    }
+    return false;
+}
+
+/*Function 7 */
+function convertMatrix(mat){
+    if(!Array.isArray(mat)){
+        return false;
+    }
+    for(var i=0; i<mat.length;i++){
+        for(var j=0; j<mat.length; j++){
+            if(mat[i][j] === 0){
+                for(var k=0; k<mat.length;k++){
+                    mat[k][j]=0;
+                    mat[i][k]=0;
+                }
+                break;
+            }
+        }
+    }
+    return mat;
+}
+
+/*Function 8 */
+function rotate(mat) {
+    if(mat instanceof Array){
+        var max = mat.length;
+        if(max > 0){
+            for(var i = 0; i < max; i++){
+                if(mat[i].length !== max){
+                    return false;
+                }
+            }
+            var res = [];
+            for(var x = 0; x < max; x++){
+                res[x] = [];
+                for(var y = max - 1; y >= 0; y--){
+                    res[x][max - 1 - y] = mat[y][x];
+                }
+            }
+            return res;
+        }
+    }
+    return false;
+}
+
+//console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]));
+
 
 /*Exercise 1 : Anagrams*/
 describe("Testing Anagram", function(){
@@ -649,5 +729,157 @@ describe("Testing squareSum", function(){
     it("Espero que squareSum de -18 sea igual a false",function(){
         var res = squareSum(-18);
         expect(res).toBe(false);
+    });
+    it("Espero que squareSum de [] sea igual a false",function(){
+        var res = squareSum([]);
+        expect(res).toBe(false);
+    });
+});
+
+/*Exercise 6*/
+describe("Testing isRotation", function(){
+    it("Espero que maro isRotation de roma",function(){
+        var res = isRotation("roma", "maro");
+        expect(res).toBe(true);
+    });
+    it("Espero que mar isRotation de roma",function(){
+        var res = isRotation("roma", "mar");
+        expect(res).toBe(false);
+    });
+    it("Espero que 2 isRotation de roma",function(){
+        var res = isRotation("roma", 2);
+        expect(res).toBe(false);
+    });
+    it("Espero que roma isRotation de 2",function(){
+        var res = isRotation(2, "roma");
+        expect(res).toBe(false);
+    });
+    it("Espero que [r,o,m,a] isRotation de [m,a,r,o]",function(){
+        var res = isRotation(["m","a","r","o"], ["r","o","m","a"]);
+        expect(res).toBe(false);
+    });
+    it("Espero que roma isRotation de [m,a,r,o]",function(){
+        var res = isRotation(["m","a","r","o"], "roma");
+        expect(res).toBe(false);
+    });
+    it("Espero que [m,a,r,o] isRotation de roma",function(){
+        var res = isRotation("roma", ["m","a","r","o"]);
+        expect(res).toBe(false);
+    });
+    it("Espero que null isRotation de null",function(){
+        var res = isRotation(null, null);
+        expect(res).toBe(false);
+    });
+    it("Espero que 2 isRotation de 2",function(){
+        var res = isRotation(2, 2);
+        expect(res).toBe(false);
+    });
+    it("Espero que NaN isRotation de roma",function(){
+        var res = isRotation("roma", NaN);
+        expect(res).toBe(false);
+    });
+    it("Espero que NaN isRotation de NaN",function(){
+        var res = isRotation(NaN, NaN);
+        expect(res).toBe(false);
+    });
+    it("Espero que {1:1, 2:2} isRotation de {1:1, 2:2}",function(){
+        var res = isRotation({1:1, 2:2}, {1:1, 2:2});
+        expect(res).toBe(false);
+    });
+    it("Espero que \"\" isRotation de \"\"",function(){
+        var res = isRotation("", "");
+        expect(res).toBe(false);
+    });
+    it("Espero que \" \" isRotation de \" \"",function(){
+        var res = isRotation(" ", " ");
+        expect(res).toBe(false);
+    });
+    it("Espero que NOLASCO isRotation de lascono",function(){
+        var res = isRotation("NOLASCO", "LASCONO");
+        expect(res).toBe(true);
+    });
+    it("Espero que -1 isRotation de axel",function(){
+        var res = isRotation(-1, "axel");
+        expect(res).toBe(false);
+    });
+});
+
+/*Exercise 7*/
+describe("Testing convertMatrix", function(){
+    it("Espero que convertMatrix [[1,1,1], [1,1,1], [1,1,0]] devuelva [[1,1,0],[1,1,0],[0,0,0]]",function(){
+        var res = convertMatrix([[1,1,1], [1,1,1], [1,1,0]]);
+        expect(res).toEqual([[1,1,0],[1,1,0],[0,0,0]]);
+    });
+    it("Espero que convertMatrix a devuelva false",function(){
+        var res = convertMatrix("a");
+        expect(res).toEqual(false);
+    });
+    it("Espero que convertMatrix 5 devuelva false",function(){
+        var res = convertMatrix(5);
+        expect(res).toEqual(false);
+    });
+    it("Espero que convertMatrix Axel devuelva false",function(){
+        var res = convertMatrix("Axel");
+        expect(res).toEqual(false);
+    });
+    it("Espero que convertMatrix {1:1,2:2,3:3} devuelva false",function(){
+        var res = convertMatrix({1:1,2:2,3:3});
+        expect(res).toEqual(false);
+    });
+});
+
+/*Exercise 8*/
+describe("Testing rotate", function(){
+    it("Espero que rotate [[1,2,3], [4,5,6], [7,8,9]] equal [[1,2,3], [4,5,6], [7,8,9]]",function(){
+        var res = rotate([[1,2,3], [4,5,6], [7,8,9]]);
+        expect(res).toEqual([[7,4,1],[8,5,2],[9,6,3]]);
+    });
+    it("Espero que rotate Axel return false", function(){
+        var res = rotate("Axel");
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate 123456789 return false", function(){
+        var res = rotate(123456789);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate {1:2,2:3,3:4} return false", function(){
+        var res = rotate(123456789);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate NaN return false", function(){
+        var res = rotate(NaN);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate false return false", function(){
+        var res = rotate(false);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate \" \" return false", function(){
+        var res = rotate(" ");
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate Infinity return false", function(){
+        var res = rotate(Infinity);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate -1 return false", function(){
+        var res = rotate(-1);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate [] return false", function(){
+        var res = rotate([]);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate [[],[],[]] return false", function(){
+        var res = rotate([[],[],[]]);
+        expect(res).toEqual(false);
+    });
+    it("Espero que rotate [[q,w,e],[1,2,3],[f,d,4]] return [['f',1,'q'], ['d',2,'w'], [4,3,'e']]", function(){
+        var res = rotate([["q","w","e"],[1,2,3],["f","d",4]]);
+        expect(res).toEqual([['f',1,'q'], ['d',2,'w'], [4,3,'e']]);
+    });
+    it("Espero que rotate [[[],[]],[[],[]],[[],[]]] return false", function(){
+        var res = rotate([[[],[]],[[],[]],[[],[]]]);
+        expect(res).toEqual(false);
     });
 });
