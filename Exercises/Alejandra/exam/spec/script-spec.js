6208f2e2a93166fd48daad41a9c9bbbb0ef752a0
ale <jandrita@live.com.mx>
2016-09-14 18:56:26 -0500

    Me faltaron la mayoria de los ejercicio, some,every y concat son los que hice

--- Exercises/Alejandra/exam/spec/script-spec.js
+++ Exercises/Alejandra/exam/spec/script-spec.js
@@ -11,13 +11,70 @@ function deepReverse(array){
   aux[0]=array.reverse(array);
   console.log(aux);
 }
-deepReverse([[1,2],[3,4],[6,7]]);
+
+function concat(array,n){
+  var arreglo=[];
+  var x;
+
+
+    for(var i=0; i<array.length; i++){
+      arreglo[i]=array[i];
+      x=arreglo.concat(n);
+    }
+    console.log(x);
+
+}
+function every(array,funct){
+  if(typeof(array)!=="Array"){
+    console.log(array);
+    return false;
+  }
+
+  if(typeof(funct)!=="objet"){
+    return false;
+  }else {
+    for(var i=0; i<array.length; i++){
+      if(funct(i,i,array)===false){
+        return false;
+      }else{
+        return true;
+      }
+    }
+  }
+}
+ console.log(every([1,2,3]));
+ function some(array,funct){
+   if(typeof(array)!=="Array"){
+     console.log(array);
+     return false;
+   }
+
+   if(typeof(funct)!=="objet"){
+     return false;
+   }else {
+     for(var i=0; i<array.length; i++){
+       if(funct(i,i,array)===true){
+         return true;
+       }else{
+         return false;
+       }
+     }
+   }
+ }
+  console.log(some([6,7,8,9]));
+
 
 describe("",function(){
   it("function",function(){
     expect(deepReverse()).toBeDefined();
   });
 });
+describe("concat\n",function(){
+  it("mando un arreglo ([1,2,3],[4,5,6,[7]]) y cualquier otro elemento y da como resultado [1,2,3,4,5,6,[7]]",function(){
+  //  expect(concat([1,2,3],[4,5,6,[7]])).toBe([1,2,3,4,5,6,[7]]);
+  });
+});
+
 describe("mando un string y espero un false",function(){
   it("Mando un string y espero un false",function(){
     expect(deepReverse("String")).toBe(false);
