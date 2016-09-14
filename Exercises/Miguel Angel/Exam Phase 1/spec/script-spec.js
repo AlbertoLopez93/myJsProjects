ff820e2b24981a0b774e3285e116ebf27bb5627f
Miguel <miguedelgado1324@gmail.com>
2016-09-14 12:55:32 -0500

    Exam phase 1 finished :)

---
+++ Exercises/Miguel Angel/Exam Phase 1/spec/script-spec.js
@@ -0,0 +1,286 @@
+"use strict"
+function add(date, num, str){
+	if(date instanceof Date && (typeof num === 'number' || num instanceof Number) && (typeof str === 'string' || str instanceof String)){
+		if(!Number.isInteger(num) || num < 0){
+			return false;
+		}
+		str.toLowerCase();
+		switch(true){
+			case str === 'years':
+				var years = date.getFullYear();
+				date.setFullYear(years + num);
+				return date;
+			case str === 'quarters':
+				var quarter = date.getMonth();
+				num *= 3;
+				date.setMonth(quarter + num);
+				return date;
+			case str === 'months':
+				var month = date.getMonth();
+				date.setMonth(month + num);
+				return date;
+			case str === 'days':
+				var day = date.getDate();
+				date.setDate(day + num);
+				return date;
+			case str === 'hours':
+				var hours = date.getHours();
+				date.setHours(hours + num);
+				return date;
+			case str === 'minutes':
+				var minutes = date.getMinutes();
+				date.setMinutes(minutes + num);
+				return date;
+			case str === 'seconds':
+				var seconds = date.getSeconds();
+				date.setSeconds(seconds + num);
+				return date;
+			case str === 'milliseconds':
+				var milliseconds = date.getMilliseconds();
+				date.setMilliseconds(milliseconds + num);
+				return date;
+			default:
+				return false;
+		}
+	} else {
+		return false;
+	}
+}
+
+describe("Tests add function", function () {
+    it("Should exists", function () {
+    	var date1 = new Date(2010, 5, 12);
+    	var dateExp = new Date(2014, 5, 12)
+    	var res = add(date1, 4, 'years');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date2 = new Date(2016, 8, 4);
+    	var dateExp = new Date(2016, 11, 4)
+    	var res = add(date2, 1, 'quarters');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date3 = new Date(2017, 8);
+    	var dateExp = new Date(2017, 10)
+    	var res = add(date3, 2, 'months');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date4 = new Date(2017, 8, 15);
+    	var dateExp = new Date(2017, 8, 21)
+    	var res = add(date4, 6, 'days');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date5 = new Date(2010, 8, 7, 8);
+    	var dateExp = new Date(2010, 8, 7, 20);
+    	var res = add(date5, 12, 'hours');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date6 = new Date(2011, 8, 8, 17, 12);
+    	var dateExp = new Date(2011, 8, 8, 17, 45);
+    	var res = add(date6, 33, 'minutes');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date7 = new Date(2012, 8, 9, 16, 30, 11);
+    	var dateExp = new Date(2012, 8, 9, 16, 30, 55);
+    	var res = add(date7, 44, 'seconds');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var dateExp = new Date(2013, 8, 10, 10, 0, 1, 729);
+    	var res = add(date8, 721, 'milliseconds');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var res = add(date8, 721, 'milliseconds1');
+    	expect(res).toEqual(false);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var res = add(date8, true, 'milliseconds');
+    	expect(res).toEqual(false);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var res = add(date8, Infinity, 'milliseconds');
+    	expect(res).toEqual(false);
+    });
+});
+
+function subtract(date, num, str){
+	if(date instanceof Date && (typeof num === 'number' || num instanceof Number) && (typeof str === 'string' || str instanceof String)){
+		if(!Number.isInteger(num) || num < 0){
+			return false;
+		}
+		str.toLowerCase();
+		switch(true){
+			case str === 'years':
+				var years = date.getFullYear();
+				date.setFullYear(years - num);
+				return date;
+			case str === 'quarters':
+				var quarter = date.getMonth();
+				num *= 3;
+				date.setMonth(quarter - num);
+				return date;
+			case str === 'months':
+				var month = date.getMonth();
+				date.setMonth(month - num);
+				return date;
+			case str === 'days':
+				var day = date.getDate();
+				date.setDate(day - num);
+				return date;
+			case str === 'hours':
+				var hours = date.getHours();
+				date.setHours(hours - num);
+				return date;
+			case str === 'minutes':
+				var minutes = date.getMinutes();
+				date.setMinutes(minutes - num);
+				return date;
+			case str === 'seconds':
+				var seconds = date.getSeconds();
+				date.setSeconds(seconds - num);
+				return date;
+			case str === 'milliseconds':
+				var milliseconds = date.getMilliseconds();
+				date.setMilliseconds(milliseconds - num);
+				return date;
+			default:
+				return false;
+		}
+	} else {
+		return false;
+	}
+}
+
+describe("Tests subtract function", function () {
+    it("Should exists", function () {
+    	var date1 = new Date(2010, 5, 12);
+    	var dateExp = new Date(2006, 5, 12)
+    	var res = subtract(date1, 4, 'years');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date2 = new Date(2016, 8, 4);
+    	var dateExp = new Date(2016, 5, 4)
+    	var res = subtract(date2, 1, 'quarters');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date3 = new Date(2017, 8);
+    	var dateExp = new Date(2017, 6)
+    	var res = subtract(date3, 2, 'months');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date4 = new Date(2017, 8, 15);
+    	var dateExp = new Date(2017, 8, 9)
+    	var res = subtract(date4, 6, 'days');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date5 = new Date(2010, 8, 7, 20);
+    	var dateExp = new Date(2010, 8, 7, 8);
+    	var res = subtract(date5, 12, 'hours');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date6 = new Date(2011, 8, 8, 17, 45);
+    	var dateExp = new Date(2011, 8, 8, 17, 12);
+    	var res = subtract(date6, 33, 'minutes');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date7 = new Date(2012, 8, 9, 16, 30, 55);
+    	var dateExp = new Date(2012, 8, 9, 16, 30, 11);
+    	var res = subtract(date7, 44, 'seconds');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 729);
+    	var dateExp = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var res = subtract(date8, 721, 'milliseconds');
+    	expect(res).toEqual(dateExp);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var res = subtract(date8, 721, 'milliseconds1');
+    	expect(res).toEqual(false);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var res = subtract(date8, true, 'milliseconds');
+    	expect(res).toEqual(false);
+    });
+    it("Should exists", function () {
+    	var date8 = new Date(2013, 8, 10, 10, 0, 1, 8);
+    	var res = subtract(date8, Infinity, 'milliseconds');
+    	expect(res).toEqual(false);
+    });
+});
+
+function deepReverse (arr){
+	if(!Array.isArray(arr)){
+		return false;
+	}
+	var len = arr.length-1;
+	var flag = Array.from(arr);
+	for(let k = 0; k < arr.length; k++){
+		if(Array.isArray(arr[k])){
+			flag[k] = true;
+		}
+	}
+	for(let i = 0; i < arr.length; i++){
+		for(let j = 0; j < len; j++){
+			if(Array.isArray(arr[j]) && flag[j] === true){
+				arr[j] = deepReverse(arr[j]);
+				flag[j] = false;
+			}
+			if(Array.isArray(arr[j+1]) && flag[j+1] === true){
+				arr[j+1] = deepReverse(arr[j+1]);
+				flag[j+1] = false;
+			}
+			var temp = 0;
+			temp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = temp;
+		}
+		len--;
+	}
+	return arr;
+}
+
+describe("Tests deepReverse function", function () {
+	it("Should exists", function () {
+    	var res = deepReverse([[1,2], [3,4]]);
+    	expect(res).toEqual([[4,3], [2,1]]);
+    });
+    it("Should exists", function () {
+    	var res = deepReverse([[12,11,10],[9,8,7], [6,5,4], [3,2,1]]);
+    	expect(res).toEqual([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]]);
+    });
+    it("Should exists", function () {
+    	var res = deepReverse([50, 51,[52, 53]]);
+    	expect(res).toEqual([[53, 52], 51, 50]);
+    });
+    it("Should exists", function () {
+    	var res = deepReverse([50, 51,[52, 53,[1,2,3,[5,6,7,[9,12]]]]]);
+    	expect(res).toEqual([[[[[12,9],7,6,5],3,2,1],53,52],51,50]);
+    });
+    it("Should exists", function () {
+    	var res = deepReverse([[9,8,7], [6,5,4], [3,2,1]]);
+    	expect(res).toEqual([[1,2,3], [4,5,6], [7,8,9]]);
+    });
+    it("Should exists", function () {
+    	var res = deepReverse([[false,true,undefined], [{},[],'foo'], [3,2,1]]);
+    	expect(res).toEqual([[1,2,3], ['foo',[],{}], [undefined,true,false]]);
+    });
+});
