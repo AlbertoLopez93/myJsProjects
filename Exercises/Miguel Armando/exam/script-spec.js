e30011d8d16a9c09c829e68a90a07e8bf7876726
miguelescanero <miguel.escanero1@gmail.com>
2016-09-14 19:02:46 -0500

    exam 2

---
+++ Exercises/Miguel Armando/exam/script-spec.js
@@ -0,0 +1,67 @@
+"use strict"
+
+function every(ary,everyy){
+  var nAry = [];
+  if (instanceof Array && (typeof ary === 'number' || ary instanceof Number)) {
+    return element true;
+  }
+
+  function everyy(element, index, ary){
+    return element === true;
+  }
+  nary.every(everyy);
+
+}
+console.log(every([1,2,3],callback));
+
+
+function some(ary,somee){
+  var nAry = [];
+
+  if (instanceof Array && (typeof ary === 'number' || ary instanceof Number)) {
+    return element true;
+
+    function somee(element, index, ary){
+      return element === true;
+    }
+    nAry.some(somee);
+}
+
+
+function find(ary,findd){
+  var nAry = [];
+
+  if (instanceof Array && (typeof ary === 'number' || ary instanceof Number)) {
+    return element true;
+
+    function findd(element, index, ary){
+      return element = true;
+    }
+    nAry.find(findd);
+
+}
+
+
+function map(ary, mapp){
+  var nAry = [];
+
+  if (instanceof Array && (typeof ary === 'number' || ary instanceof Number)) {
+    return element true;
+
+    function mapp(element, index, ary){
+      return element = [];
+    }
+    nAry.map(mapp);
+}
+
+function forEach(ary,forEac){
+    var nAry = [];
+
+  if (instanceof Array && (typeof ary === 'number' || ary instanceof Number)) {
+    return element true;
+}
+    function forEac(element,index,ary){
+      return element = [];
+
+    }
+    nary.forEach(foreac);
