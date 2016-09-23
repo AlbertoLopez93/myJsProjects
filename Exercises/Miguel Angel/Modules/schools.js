f4e25bc33ecf73b486e05e6b483a0cd612191110
Miguel <miguedelgado1324@gmail.com>
2016-09-22 19:35:34 -0500

    Modules exercises, modules of calculator and modules of JSON Schools

---
+++ Exercises/Miguel Angel/Modules/schools.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let schools = require('../../Data/schools');
+let School = require('./school').School;
+
+var result = schools.map(school => {return new School(school);});
+console.log(result);
