5515c99737753a0620a441719fdd5de7cff0d5fc
Miguel <miguedelgado1324@gmail.com>
2016-09-26 20:12:11 -0500

    Constructor Student and constructor Friend heredates methods from constructor Human

--- Exercises/Miguel Angel/Modules/friend.js
+++ Exercises/Miguel Angel/Modules/friend.js
@@ -1,9 +1,16 @@
 "use strict"
 
+let Human = require('./human').Human;
+
 function Friend (friend){
 	this.id = friend.id;
-	this.name = friend.name;
+	var name = friend.name.split(' ');
+	this.firstName = name[0];
+	this.lastName = name[1];
 }
+
+Friend.prototype = new Human();
+Friend.prototype.constructor = Friend;
 
 module.exports = {
 	Friend: Friend
