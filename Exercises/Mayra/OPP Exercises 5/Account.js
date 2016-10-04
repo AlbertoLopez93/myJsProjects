2cd39232e6af5de52235ae654ea510e616d190b6
Mayra Janette Huerta Carbajal <mayra.huerta@alumnos.udg.com>
2016-10-03 17:23:12 -0700

    ejercicios opp 5

--- Exercises/Mayra/OPP Exercises 5/Account.js
+++ Exercises/Mayra/OPP Exercises 5/Account.js
@@ -1,9 +1,13 @@
 "use strict";
 let  NewCustomer=require("./NewCustomer.js");
 function Account(id,customer,balance) {
-    let balance=balance||0.0;
+
+    if (balance===undefined) {
+        balance=0.0;
+
+    }
     this.getID=function getID() {
-        return ID;
+        return id;
     }
     this.getCustomer=function getCustomer() {
         return customer;
@@ -18,7 +22,7 @@ function Account(id,customer,balance) {
     }
 }
 Account.prototype.toString = function toString() {
-    let str="name("+this.getID()+" balance=$"+thi.getBalance().toFixed(); +")";
+    let str="name("+this.getID()+" balance=$"+this.getBalance().toFixed(2)+")";
     return str;
 };
 Account.prototype.getCustomerName=function getCustomerName() {
@@ -29,19 +33,34 @@ Account.prototype.deposit=function deposit(amount) {
     if (typeof amount==="number"&&amount>0){
         let result=this.getBalance()+amount;
         this.setBalance(result);
-        return this
+        return this;
     }
 }
 Account.prototype.withdraw=function withdraw(amount) {
+    let msj="Amount withdraw exceeds the current balance";
         if (amount<=this.getBalance()) {
             let subtract=this.getBalance()-amount;
             this.setBalance(subtract);
             return this;
 
         }else {
-
+            console.log(msj);
             return this;
         }
-}
-
+}/*
+var cus= new NewCustomer(111,"mayra","f");
+var prueba= new Account(5555,cus,52.0);
+var prueba2= new Account(2222,cus);
+console.log(prueba.getID());
+console.log(prueba.getBalance());
+console.log(prueba2.getID());
+console.log(prueba2.getBalance());
+prueba.setBalance(100.2233);
+console.log(prueba.getBalance());
+console.log(prueba.toString());
+console.log(prueba.getCustomerName());
+console.log(prueba.deposit(100));
+console.log(prueba.getBalance());
+console.log(prueba.withdraw(100));
+console.log(prueba.getBalance());*/
 module.exports=Account;
