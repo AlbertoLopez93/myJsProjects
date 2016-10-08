0b27356be5eb987e470cb48a0597b02cd5045c0b
EddyVelazquez <eddy.jaziel.velazquez.gomez@gmail.com>
2016-10-08 17:27:25 -0500

    validate against brauli's probes + negative numbers

--- Exercises/Eddy/Best Practices/abbreviateNumber.js
+++ Exercises/Eddy/Best Practices/abbreviateNumber.js
@@ -1,7 +1,37 @@
 /**
- * Created by jazielsama on 7/10/16.
- *
+ * Created by eddy velaquez on 7/10/16.
  */
 
 "use strict";
-/*jshint esversion: 6 */
+
+function abbreviateNumber(num) {
+
+	function positive() {
+
+		num = num.toString();
+		let len = num.length;
+		if(len < 4) {
+			return num;
+		} else if(len < 5 ) {
+			return num.slice(0, -3) + "," + num.slice(1);
+		} else if(len < 6) {
+			return num.slice(0, -3) + "." + num.slice(2, 3) + "K";
+		} else if(len < 7 && num < 999500) {
+			return num.slice(0, -3) + "K";
+		} else if(len < 7 && num < 1010000) {
+			return "1.00M";
+		} else if(len < 9) {
+			return num.slice(0, -6) + "." + num.slice(-6, -4) + "M";
+		} else {
+			return num.slice(0, -6) + "M";
+		}
+	}
+	if (num >= 0){
+		return positive();
+	}else {
+		num = num*-1;
+		return "-"+positive();
+	}
+}
+
+module.exports = abbreviateNumber;
--- Exercises/Eddy/Best Practices/spec/abbreviateNumber-spec.js
+++ Exercises/Eddy/Best Practices/spec/abbreviateNumber-spec.js
@@ -1,10 +1,61 @@
 /****************************************************************************
 
-Name: abbreviateNumber.js
-Search: label:interns {subject:"arrays ex 1" subject:"arrays ex1" subject:arraysex1} 
-Target: Exercises/$1/Arrays
-Function: abbreviateNumber
+ Name: abbreviateNumber.js
+ Search: label:interns {subject:"arrays ex 1" subject:"arrays ex1" subject:arraysex1}
+ Target: Exercises/$1/Arrays
+ Function: abbreviateNumber
 
-****************************************************************************/
+ ****************************************************************************/
+"use strict";
 
-describe("Testing Large numbers exercise",function(){var a=require("../abbreviateNumber"),b=[[101e7,"1010M"],[101e4,"1.01M"],[1001e3,"1.00M"],[1000500,"1.00M"],[1e6,"1.00M"],[999999,"1.00M"],[999500,"1.00M"],[999499,"999K"],[999001,"999K"],[1e4,"10.0K"],[9999,"9,999"],[888,"888"]];b.forEach(function(b){it("The number is "+b[0],function(){expect(a(b[0])).toBe(b[1])})})});
+const abbreviateNumber = require("../abbreviateNumber");
+const ary = [
+	[999999e6,"999999M"],
+	[999e7, "9990M"],
+	[101e7, "1010M"],
+	[999e5, "99.90M"],
+	[101e5, "10.10M"],
+	[101e4, "1.01M"],
+	[1230000, "1.23M"],
+	[1001e3, "1.00M"],
+	[1000500, "1.00M"],
+	[1e6, "1.00M"],
+	[999999, "1.00M"],
+	[999500, "1.00M"],
+	[999499, "999K"],
+	[999001, "999K"],
+	[12345, "12.3K"],
+	[1e4, "10.0K"],
+	[9999, "9,999"],
+	[1299, "1,299"],
+	[888, "888"],
+	[0, "0"],
+	[-999e7, "-9990M"],
+	[-101e7, "-1010M"],
+	[-999e5, "-99.90M"],
+	[-101e5, "-10.10M"],
+	[-101e4, "-1.01M"],
+	[-1230000, "-1.23M"],
+	[-1001e3, "-1.00M"],
+	[-1000500, "-1.00M"],
+	[-1e6, "-1.00M"],
+	[-999999, "-1.00M"],
+	[-999500, "-1.00M"],
+	[-999499, "-999K"],
+	[-999001, "-999K"],
+	[-12345, "-12.3K"],
+	[-1e4, "-10.0K"],
+	[-9999, "-9,999"],
+	[-1234, "-1,234"],
+	[10100, "10.1K"],
+	[10053, "10.0K"],
+	[10049, "10.0K"],
+	[9948, "9,948"]];
+
+describe("Testing Large numbers exercise", function() {
+	ary.forEach(function(element) {
+		it("The number is " + element[0], function() {
+			expect(abbreviateNumber(element[0])).toBe(element[1]);
+		});
+	});
+});
