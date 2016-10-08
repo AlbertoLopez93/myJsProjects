56575f2892642927412de36b435383fd9352f300
EddyVelazquez <eddy.jaziel.velazquez.gomez@gmail.com>
2016-10-08 18:31:21 -0500

    added toFixed to all values

--- Exercises/Eddy/Best Practices/abbreviateNumber.js
+++ Exercises/Eddy/Best Practices/abbreviateNumber.js
@@ -1,5 +1,6 @@
 /**
- * Created by eddy velaquez on 7/10/16.
+ * Created by eddy velaquez on 7/10/16. BEST PRECTICES
+ *
  */
 
 "use strict";
@@ -12,13 +13,16 @@ function abbreviateNumber(num) {
 		let len = num.length;
 		if(len < 4) {
 			return num;
-		} else if(len < 5 ) {
+		} else if(len < 5) {
 			return num.slice(0, -3) + "," + num.slice(1);
 		} else if(len < 6) {
+			num = ((num / 1000).toFixed(2) * 1000).toString();
 			return num.slice(0, -3) + "." + num.slice(2, 3) + "K";
 		} else if(len < 7 && num < 999500) {
+			num = ((num / 1000).toFixed(2) * 1000).toString();
 			return num.slice(0, -3) + "K";
-		} else if(len < 7 && num < 1010000) {
+		} else if(len < 7) {
+			num = ((num / 1000000).toFixed(2) * 1000000).toString();
 			return "1.00M";
 		} else if(len < 9) {
 			return num.slice(0, -6) + "." + num.slice(-6, -4) + "M";
@@ -26,11 +30,12 @@ function abbreviateNumber(num) {
 			return num.slice(0, -6) + "M";
 		}
 	}
-	if (num >= 0){
+
+	if(num >= 0) {
 		return positive();
-	}else {
-		num = num*-1;
-		return "-"+positive();
+	} else {
+		num = num * -1;
+		return "-" + positive();
 	}
 }
 
