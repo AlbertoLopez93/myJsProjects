5ccac838002ea95a63788985eaa4b5717f825e7e
Miguel Delgado <miguedelgado1324@gmail.com>
2016-10-08 19:58:15 -0500

    Corrected function abbreviateNumber after try it with other tests

--- Exercises/Miguel Angel/Best Practices/abbreviateNumber.js
+++ Exercises/Miguel Angel/Best Practices/abbreviateNumber.js
@@ -5,27 +5,13 @@ function abbreviateNumber(largeNumber){
     if(largeNumber < 1000){
         largeNumberStr = "" + largeNumber;
     } else if(largeNumber < 10000){
-        largeNumberStr = Math.floor(largeNumber / 1000) + "," +
-                         largeNumber % 1000;
+        largeNumberStr = Math.floor(largeNumber / 1000) + "," + largeNumber % 1000;
     } else if(largeNumber < 999500){
-        if(largeNumber < 99999){
-            largeNumberStr = Math.floor(largeNumber / 1000) + "." +
-                            (Math.floor((largeNumber % 1000)/10)) + "K";
-        } else {
-            largeNumberStr = Math.floor(largeNumber / 1000) + "K";
-        }
-    } else if(largeNumber < 1010000){
-        largeNumberStr = "1.00M";
+        largeNumberStr = (largeNumber / 1000).toPrecision(3) + "K";
     } else if(largeNumber < 9999999){
-        if(largeNumber < 1100000){
-            largeNumberStr = Math.floor(largeNumber / 1000000) + ".0" +
-                            (Math.floor((largeNumber % 1000000)/10000)) + "M";
-        } else {
-            largeNumberStr = Math.floor(largeNumber / 1000000) + "." +
-                            (Math.floor((largeNumber % 1000000)/10000)) + "M";
-        }
+        largeNumberStr = (largeNumber / 1000000).toPrecision(3) + "M";
     } else {
-        largeNumberStr = Math.floor(largeNumber / 1000000) + "M";
+        largeNumberStr = (largeNumber / 1000000).toPrecision(4) + "M";
     }
     return largeNumberStr;
 }
