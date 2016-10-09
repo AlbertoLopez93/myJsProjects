2668de7dfe3c78d71a42d42e2df871d70eee3ab0
Miguel Delgado <miguedelgado1324@gmail.com>
2016-10-08 20:50:28 -0500

    Function abbreviateNumber corrected, error was in under 10000 numbers

--- Exercises/Miguel Angel/Best Practices/abbreviateNumber.js
+++ Exercises/Miguel Angel/Best Practices/abbreviateNumber.js
@@ -1,17 +1,24 @@
 "use strict";
 
 function abbreviateNumber(largeNumber){
-    let largeNumberStr;
+    let largeNumberStr = "";
+    if(largeNumber < 0){
+        largeNumberStr = "-";
+        largeNumber *= -1;
+    }
     if(largeNumber < 1000){
-        largeNumberStr = "" + largeNumber;
+        largeNumberStr += "" + largeNumber;
     } else if(largeNumber < 10000){
-        largeNumberStr = Math.floor(largeNumber / 1000) + "," + largeNumber % 1000;
+        largeNumberStr += Math.floor(largeNumber / 1000) + "," +
+                          (Math.floor((largeNumber % 1000) / 100)).toPrecision(1) +
+                          (Math.floor((largeNumber % 100) / 10)).toPrecision(1) +
+                          (Math.floor((largeNumber % 10) / 1)).toPrecision(1);
     } else if(largeNumber < 999500){
-        largeNumberStr = (largeNumber / 1000).toPrecision(3) + "K";
+        largeNumberStr += (largeNumber / 1000).toPrecision(3) + "K";
     } else if(largeNumber < 9999999){
-        largeNumberStr = (largeNumber / 1000000).toPrecision(3) + "M";
+        largeNumberStr += (largeNumber / 1000000).toPrecision(3) + "M";
     } else {
-        largeNumberStr = (largeNumber / 1000000).toPrecision(4) + "M";
+        largeNumberStr += (largeNumber / 1000000).toPrecision(4) + "M";
     }
     return largeNumberStr;
 }
