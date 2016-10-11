2815f80a9b64d4b50b345850380e9fa605e8f84d
Miguel Delgado <miguedelgado1324@gmail.com>
2016-10-10 19:19:51 -0500

    Function abbreviateNumber passes all the trainer's tests, correction of trainer's tests

--- Exercises/Miguel Angel/Best Practices/abbreviateNumber.js
+++ Exercises/Miguel Angel/Best Practices/abbreviateNumber.js
@@ -2,25 +2,28 @@
 
 function abbreviateNumber(largeNumber){
     let largeNumberStr = "";
-    if(largeNumber < 0){
-        largeNumberStr = "-";
-        largeNumber *= -1;
-    }
-    if(largeNumber < 1000){
+    let unity = ["K", "M"];
+    let values = [1000, 10000, 999500, 100000000, 1000000000];
+
+    if(largeNumber < values[0]){
         largeNumberStr += "" + largeNumber;
-    } else if(largeNumber < 10000){
-        largeNumberStr += Math.floor(largeNumber / 1000) + "," +
-                          (Math.floor((largeNumber % 1000) / 100)).toPrecision(1) +
-                          (Math.floor((largeNumber % 100) / 10)).toPrecision(1) +
-                          (Math.floor((largeNumber % 10) / 1)).toPrecision(1);
-    } else if(largeNumber < 999500){
-        largeNumberStr += (largeNumber / 1000).toPrecision(3) + "K";
-    } else if(largeNumber < 9999999){
-        largeNumberStr += (largeNumber / 1000000).toPrecision(3) + "M";
+    } else if(largeNumber < values[1]){
+        largeNumberStr += (Math.floor(largeNumber / 1000)) + "," +
+                          (Math.floor((largeNumber % 1000) / 100)) +
+                          (Math.floor((largeNumber % 100) / 10)) +
+                          ((largeNumber % 10) / 1).toPrecision(1);
+    } else if (largeNumber < values[2]) {
+        largeNumberStr += (largeNumber / 1000).toPrecision(3) + unity[0];
+    } else if (largeNumber < values[3]) {
+        largeNumberStr += (largeNumber / 1000000).toPrecision(3) + unity[1];
+    } else if (largeNumber < values[4]) {
+        largeNumberStr += (largeNumber / 1000000).toPrecision(3) + unity[1];
     } else {
-        largeNumberStr += (largeNumber / 1000000).toPrecision(4) + "M";
+        largeNumberStr += (largeNumber / 1000000).toPrecision(4) + unity[1];
     }
+
     return largeNumberStr;
+
 }
 
 module.exports = abbreviateNumber;
