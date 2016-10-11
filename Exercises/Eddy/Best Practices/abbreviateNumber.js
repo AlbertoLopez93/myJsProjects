e460ab2dd1cee3cca0020a27c7759d3945d899bb
EddyVelazquez <eddy.jaziel.velazquez.gomez@gmail.com>
2016-10-10 19:32:40 -0500

    functions passed all tests

--- Exercises/Eddy/Best Practices/abbreviateNumber.js
+++ Exercises/Eddy/Best Practices/abbreviateNumber.js
@@ -10,28 +10,26 @@ function abbreviateNumber(num) {
 }
 
 function positive(num) {
-	let keys = ['K', 'M'];                          // kilos & Megas
-	let ran = [1000, 10000, 999500, 100000000];     //ranges
-	let den = [1000, 1000000];                      // denomitors
+	let keys = ['K', 'M'];                                      // kilos & Megas
+	let ran = [1000, 10000, 999500, 100000000, 1000000000];     //ranges
+	let den = [1000, 1000000];                                  // denomitors
 
 	if(num < ran[0]) {
-		num = num.toPrecision(2);
+		num = num.toString();
 	} else if(num < ran[1]) {
 		num = num.toPrecision(4);
 		num = num.slice(0, 1) + ',' + num.slice(1);
 	} else if(num < ran[2]) {
-		num = (num / den[0]).toPrecision(3);
-		num = num + keys[0];
+		num = (num / den[0]).toPrecision(3)+ keys[0];
 	} else if(num < ran[3]) {
-		num = (num / den[1]).toPrecision(3);
-		num = num + keys[1];
-	} else {
-		num = Math.floor(num / den[1]) + keys[1];
+		num = (num / den[1]).toPrecision(3)+keys[1];
+	} else if(num < ran[4]){
+		num = (num / den[1]).toPrecision(3) + keys[1];
+	}else {
+		num = (num / den[1]).toPrecision(4) + keys[1];
 	}
 
 	return num;
 }
 
 module.exports = abbreviateNumber;
-
-console.log(abbreviateNumber(999999));
