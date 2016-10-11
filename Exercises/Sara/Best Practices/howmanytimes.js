e5bff84b78482bf1fcad3166dd74ad43f55687b9
fuksia <sara.fuksia@hotmail.com>
2016-10-10 19:31:37 -0500

    Correciones con los espacios en los operadores, y preubas un poco mas descriptivas

--- Exercises/Sara/Best Practices/howmanytimes.js
+++ Exercises/Sara/Best Practices/howmanytimes.js
@@ -1,26 +1,26 @@
-/*The objective is to know how many days you will have to go Six Flags, for example,
+/*The objective is to know how many days you will have to go to Six Flags (for example)
 so you can take advantage of an annual pass instead of buy a one day pass everytime you go*/
 "use strict";
-function howManyTimes(annualPass,oneDayPass){
-  if(annualPass === 0 || oneDayPass === 0){
-    return 0;
-  }
-
+function howManyTimes(annualPassPrice, oneDayPassPrice){
   let timesYouHaveToGo;
 
-  timesYouHaveToGo = annualPass/oneDayPass;
-
-  if(timesYouHaveToGo >= 364){ //returns zero when it's not convenient for you to buy an annual pass
+  if (annualPassPrice === 0 || oneDayPassPrice === 0){
     return 0;
   }
 
-  if(timesYouHaveToGo%1===0){
-    timesYouHaveToGo=timesYouHaveToGo+1;
+  timesYouHaveToGo = annualPassPrice / oneDayPassPrice;
+
+  if (timesYouHaveToGo >= 364){ //returns zero when it's not convenient for you to buy an annual pass
+    return 0;
   }
-  else{
-    timesYouHaveToGo=Math.round(timesYouHaveToGo+.4);
+
+  if (timesYouHaveToGo % 1 === 0){
+    timesYouHaveToGo = timesYouHaveToGo + 1;
+  } else {
+    timesYouHaveToGo = Math.round(timesYouHaveToGo + 0.4);
   }
 
   return timesYouHaveToGo;
 }
-module.exports=howManyTimes;
+
+module.exports = howManyTimes;
