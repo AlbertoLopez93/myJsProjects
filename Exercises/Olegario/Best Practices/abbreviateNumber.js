acd2d9011685ec08795c9b5919b93d56df139816
shockolegario <olegario_90_shock@hotmail.com>
2016-10-10 20:28:16 -0700

    abreviateNumber_corrected

--- Exercises/Olegario/Best Practices/abbreviateNumber.js
+++ Exercises/Olegario/Best Practices/abbreviateNumber.js
@@ -1,12 +1,16 @@
 "use strict";
 function verifica(value) {
-  let abreviatura, valueToArray = "", size, negativo = false;
+  let abreviatura, valueToArray = "", size, float = false;
 
-  if (Number.isInteger(value)) {
+  if (typeof(value) === "number") {
 
-    if (value < 0) {
-      value    = value * (-1);
-      negativo = true;
+    if ((value % 1) !== 0) {
+      value = "" + Math.round(value);
+      valueToArray = value.split(".");
+      value = parseInt(valueToArray[0]);
+      valueToArray = "";
+      console.log(value);
+      float = true;
     }
 
     if (value <= 999) {
@@ -15,14 +19,15 @@ function verifica(value) {
 
     if ((value >= 1000) && (value <= 9999)) {
       valueToArray += value;
-      size         =  valueToArray.length - 1;
       valueToArray =  valueToArray.split("");
+      size         = (float) ? valueToArray.indexOf(".") : valueToArray.length -1;
+      console.log(valueToArray);
       abreviatura  =  valueToArray.slice(0, size - 2).join("") + "," + valueToArray.slice(-3).join("");
     }
 
     if ((value >= 10000) && (value <= 99999)) {
       value        =  value / 100;
-      value        =  value.toFixed(1);
+      value        =  Math.round(value);
       value        =  value * 100;
       valueToArray += value;
       size         =  valueToArray.length - 1;
@@ -32,7 +37,7 @@ function verifica(value) {
 
     if ((value >= 100000) && (value <= 999499)) {
       value        =  value / 1000;
-      value        =  value.toFixed(3);
+      value        =  Math.round(value);
       value        =  value * 1000;
       valueToArray += value;
       size         =  valueToArray.length - 1;
@@ -45,29 +50,48 @@ function verifica(value) {
     }
 
     if ((value >= 1010000) && (value <= 999999999)) {
-      value        =  value / 10000;
-      value        =  value.toFixed(2);
-      value        =  value * 10000;
-      valueToArray += value;
-      size         =  valueToArray.length - 1;
-      valueToArray =  valueToArray.split("");
-      abreviatura  =  valueToArray.slice(0, size - 5).join("") + "." + valueToArray.slice(size - 5, size - 3).join("") + "M";
+      if (value > 99999999) {
+        console.log("entre");
+        console.log(value);
+        value        =  value / 1000000;
+        value        =  Math.round(value);
+        value        =  value * 1000000;
+        valueToArray += value;
+        valueToArray =  valueToArray.split("");
+        size         =  valueToArray.length - 1;
+        abreviatura  =  valueToArray.slice(0, 3).join("") + "M";
+      }else {
+          value        =  value / 100;
+          value        =  value.toPrecision(3);
+          value        =  value * 100;
+          valueToArray += value;
+          valueToArray =  valueToArray.split("");
+          size         =  valueToArray.length - 1;
+          if (value > 9999999) {
+            abreviatura  =  valueToArray.slice(0, size - 5).join("") + "." + valueToArray.slice(size - 5, size - 4).join("") + "M";
+          } else {
+            abreviatura  =  valueToArray.slice(0, size - 5).join("") + "." + valueToArray.slice(size - 5, size - 3).join("") + "M";
+          }
+
+      }
+
     }
 
     if (value >= 1010000000) {
-      value        =  value / 100000;
-      value        =  value.toFixed(2);
-      value        =  value * 100000;
+      value        =  value / 1000000;
+      value        =  Math.round(value);
+      value        =  value * 1000000;
       valueToArray += value;
       size         =  valueToArray.length - 1;
       valueToArray =  valueToArray.split("");
       abreviatura  =  valueToArray.slice(0, size - 5).join("") + "M";
     }
 
-    abreviatura = (negativo) ? "-" + abreviatura : abreviatura;
   }
 
   return abreviatura;
 }
 
+
+console.log(verifica(878840842.28));
 module.exports = verifica;
