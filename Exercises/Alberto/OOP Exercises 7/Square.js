2e8726e6e2a43c1f2972f6f1978d3ab898b3948f
AlbertoLopez93 <beto.lv.tepatitlan@gmail.com>
2016-10-20 20:09:21 -0500

    Class Square is added and improved

--- Exercises/Alberto/OOP Exercises 7/Square.js
+++ Exercises/Alberto/OOP Exercises 7/Square.js
@@ -1,12 +1,13 @@
 "use strict";
 
 let Rectangle = require ( "./Rectangle" );
+let Shape = require ( "./Shape" );
 
 function Square ( side, color, filled ) {
   if ( typeof side !== "number" ) { //not parameters
     side = 1;
   }
-  Rectangle.call ( this, side, side, color, filled );
+  Shape.call ( this, color, filled );
 
   Object.defineProperty ( this, 'getSide',
   { value: function ( ) {
@@ -21,6 +22,34 @@ function Square ( side, color, filled ) {
     } ,
     enumerable: true, configurable: false, writable: false
   } );
+
+  Object.defineProperty( this, "setWidth",
+    { value: function(line) {
+      this.setSide(line);
+    },
+      enumerable: true, configurable: false, writable: false
+  });
+
+  Object.defineProperty( this, "setLength",
+    { value: function(line) {
+      this.setSide(line);
+    },
+      enumerable: true, configurable: false, writable: false
+  });
+
+  Object.defineProperty( this, "getWidth",
+    { value: function() {
+      return this.getSide();
+    },
+      enumerable: true, configurable: false, writable: false
+  });
+
+  Object.defineProperty( this, "getLength",
+    { value: function() {
+      return this.getSide();
+    },
+      enumerable: true, configurable: false, writable: false
+  });
 
 }
 
