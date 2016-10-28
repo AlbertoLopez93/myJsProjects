9935bd765b4efa20e3746f44db4a33915ffca228
AlbertoLopez93 <beto.lv.tepatitlan@gmail.com>
2016-10-28 13:54:05 -0500

    My last modifications

--- Exercises/Alberto/Exam 2 v2/Player.js
+++ Exercises/Alberto/Exam 2 v2/Player.js
@@ -1,36 +1,60 @@
 "use strict"
 
-let Character = require("./Character");
+let Human = require("./Human");
 
-function Animal( ID, name, maxHP, CurrentHP, hostileToPlayer, lootDropped, maxDmg, minDmg ) {
-  hostileToPlayer = hostileToPlayer || false;
-  Character.call( this, ID, name, maxHP, CurrentHP );
+function Player( ID, name, maxHP, CurrentHP,
+  agility, armorEquipped, currentMP, faction, gold, intellect,
+  isMale, maxMP, spirit, stamina, strength, weaponEquipped,
+  experience, level, SP ) {
 
-  Object.defineProperty( this, "isItHostileToPlayer", {
+  Human.call( this, ID, name, maxHP, CurrentHP,
+    agility, armorEquipped, currentMP, faction, gold, intellect,
+    isMale, maxMP, spirit, stamina, strength, weaponEquipped );
+
+  Object.defineProperty( this, "getExperience", {
     value: function() {
-      return hostileToPlayer;
+      return experience;
     },
     enumerable: true
   });
-  Object.defineProperty( this, "getLootDropped", {
+  Object.defineProperty( this, "getLevel", {
     value: function() {
-      return lootDropped;
+      return level;
     },
     enumerable: true
   });
-  Object.defineProperty( this, "getMaxDmg", {
+  Object.defineProperty( this, "getSP", {
     value: function() {
-      return maxDmg;
+      return SP;
     },
     enumerable: true
   });
-  Object.defineProperty( this, "getMinDmg", {
-    value: function() {
-      return minDmg;
+  Object.defineProperty( this, "setExperience", {
+    value: function(newValue) {
+      if(typeof === "number" && newValue >= 0) {
+        experience = newValue;
+      }
+      return this;
+    },
+    enumerable: true
+  });
+  Object.defineProperty( this, "setLevel", {
+    value: function(newValue) {
+      if(typeof === "number" && newValue >= 0) {
+        level = newValue;
+      }
+    },
+    enumerable: true
+  });
+  Object.defineProperty( this, "setSP", {
+    value: function(newValue) {
+      if(typeof === "number" && newValue >= 0) {
+        SP = newValue;
+      }
     },
     enumerable: true
   });
 
 }
 
-module.exports = Animal;
+module.exports = Player;
