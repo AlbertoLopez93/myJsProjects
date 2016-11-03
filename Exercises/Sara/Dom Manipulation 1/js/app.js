32b2f4a32ea16031e27048c68c4f417e01bacbb9
fuksia <sara.fuksia@hotmail.com>
2016-11-02 23:56:50 -0600

    Star rating, me falto lo del clic :(

---
+++ Exercises/Sara/Dom Manipulation 1/js/app.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+  // $( "body" ).append("<h1>Hello</h1>");
+  $( "body" ).append( "<h1>Hello World</h1>" );
+  $( "body" ).append( "<p>" );
+  $( "p" ).append( "<span> Have </span>" );
+  $( "p" ).append( "<br>" );
+  $( "p" ).append( "<span> a nice </span>" );
+  $( "p" ).append( "<br>" );
+  $( "p" ).append( " <span>day!</span> " );
+  $( "p" ).append( "</p>" );
+// });
