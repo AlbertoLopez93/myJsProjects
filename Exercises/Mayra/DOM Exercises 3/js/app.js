05b8818c22815a36e8bac25215329d9bd99c84af
Aryamjhc <mayra.huerta@alumnos.udg.com>
2016-11-02 22:53:38 -0700

    lo que tengo

--- Exercises/Mayra/DOM Exercises 3/js/app.js
+++ Exercises/Mayra/DOM Exercises 3/js/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
+    $(".glyphicon").replaceWith("<span class='glyphicon glyphicon-star-empty'></span>");
 
-$( ".ratings").click(function() {
-  console.log("ya");
-});
+    $(".glyphicon").hover(function(){
+        $(this).prevAll().andSelf().replaceWith("<span class='glyphicon glyphicon-star'></span>");
+    });
+
+    $(".glyphicon").click(function(){
+        $(this).off('hover');
+        $(this).prevAll().replaceWith("<span class='glyphicon glyphicon-star'></span>");
+    });
+
 
 })
